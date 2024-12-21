1953f153cefa381ec2f836a2db51a749b2858ef2
Neville Nazerane <live.neville@live.com>
2024-12-20 23:43:48 -0700

    auth client

---
+++ src/Auth.ApiConsumer/AuthApiHandler.cs
@@ -0,0 +1,27 @@
+﻿using Auth.WebAPI.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.ApiConsumer
+{
+    public class AuthApiHandler : HttpClientHandler
+    {
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+                                                                     CancellationToken cancellationToken)
+        {
+            var res = await base.SendAsync(request, cancellationToken);
+            if (res.StatusCode == System.Net.HttpStatusCode.BadRequest)
+            {
+                var errors = await res.Content.ReadFromJsonAsync<IEnumerable<string>>(cancellationToken: cancellationToken) ?? [];
+                throw new BadRequestException(errors);
+            }
+            return res;
+        }
+
+    }
+}
---
+++ src/Auth.ApiConsumer/AuthClient.cs
@@ -0,0 +1,23 @@
+﻿using Auth.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.ApiConsumer
+{
+    public class AuthClient(HttpClient httpClient)
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+
+        public async Task SignupAsync(SignupModel model)
+        {
+            using var res = await _httpClient.PostAsJsonAsync("signup", model);
+            res.EnsureSuccessStatusCode();
+        }
+
+    }
+}
