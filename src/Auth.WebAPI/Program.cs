2464f6ebafba75c951732981442703f90a40f751
Neville Nazerane <live.neville@live.com>
2024-12-25 04:24:26 -0700

    restricted swagger to development

--- src/Auth.WebAPI/Program.cs
+++ src/Auth.WebAPI/Program.cs
@@ -9,9 +9,10 @@ var configs = builder.Configuration;
 var services = builder.Services;
 
 services.SetupJwt(configs.GetSection("auth"))
-        .AddDbWithIdentity(configs["sqlConnString"])
-        
-        .AddOpenApi("v1");
+        .AddDbWithIdentity(configs["sqlConnString"]);
+
+if (builder.Environment.IsDevelopment())
+    services.AddOpenApi("v1");
 
 var app = builder.Build();
 
@@ -21,7 +22,8 @@ app.MapGet("/", () => "Hello Auth!");
 
 app.MapAllEndpoints();
 
-app.MapOpenApi("/docs");
+if (builder.Environment.IsDevelopment())
+    app.MapOpenApi("/docs");
 
 await app.RunAsync();
 
